9e9085ebe7812ff079a5d2c4c4300ef204082d40
aidanwang0309 <aidanwang9303@gmail.com>
2018-08-07 14:29:10 -0400

    card section add

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,158 @@
-import React, { Component }from 'react';
+import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import IconButton from '@material-ui/core/IconButton';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import classnames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
 
-class Card extends Component {
-
-    render() {
-
-        return(
-            <li className='bg-light-green dib br3 pa3 ma2 dim fl w-30'>
-                <h3>{ this.props.name} </h3>
-                <p>Time: { this.props.time} </p>
-                <p>Distance : { this.props.distance} mile </p>
-
-                {(() => {
-                    if(this.props.price === 0){
-                            return (<p>Price Not Provided</p>);
-                        } else {
-                            return (<p>Price: ${ this.props.price.min } - ${ this.props.price.max }</p>)
-                        }
-
-                        })()}
-                <p> Location : { this.props.locationname}</p>
-                <p> Address :  {this.props.locationaddress} </p>
-            </li>
-        );
+const styles = theme => ({
+  card: {
+    maxWidth: 400
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%' // 16:9
+  },
+  actions: {
+    display: 'flex'
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest
+    }),
+    marginLeft: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginRight: -8
     }
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)'
+  },
+  avatar: {
+    backgroundColor: red[500]
+  },
+  pLeft: {
+    paddingLeft: 20
+  }
+});
+
+class EventCard extends Component {
+  state = { expanded: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card style={{ maxWidth: 400, marginTop: 50 }}>
+        <Grid item style={{ maxWidth: 345 }}>
+          <CardHeader
+            noWrap
+            title={this.props.name}
+            subheader={this.props.time}
+            action={
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            }
+          />
+          <CardMedia
+            style={{ paddingTop: '56.25%' }}
+            image={this.props.media}
+          />
+
+          <Typography
+            style={{ paddingTop: 30 }}
+            gutterBottom
+            className={classes.pLeft}
+            component="p"
+            variant="subheading"
+          >
+            {this.props.distance} mile from you
+          </Typography>
+
+          {(() => {
+            if (this.props.price === 0) {
+              return (
+                <Typography
+                  className={classes.pLeft}
+                  gutterBottom
+                  component="p"
+                  variant="subheading"
+                >
+                  Price: not provided;
+                </Typography>
+              );
+            } else {
+              return (
+                <Typography
+                  className={classes.pLeft}
+                  gutterBottom
+                  component="p"
+                  variant="subheading"
+                >
+                  Price: ${this.props.price.min} - ${this.props.price.max}
+                </Typography>
+              );
+            }
+          })()}
+
+          <CardActions className={classes.actions} disableActionSpacing>
+            <IconButton aria-label="Add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="Share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={classnames(classes.expand, {
+                [classes.expandOpen]: this.state.expanded
+              })}
+              onClick={this.handleExpandClick}
+              aria-expanded={this.state.expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+
+          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+            <Typography
+              className={classes.pLeft}
+              gutterBottom
+              component="p"
+              variant="subheading"
+            >
+              Location : {this.props.locationname}
+            </Typography>
+
+            <Typography
+              className={classes.pLeft}
+              gutterBottom
+              component="p"
+              variant="subheading"
+            >
+              Address : {this.props.locationaddress}
+            </Typography>
+          </Collapse>
+        </Grid>
+      </Card>
+    );
+  }
 }
 
-export default Card;
+export default withStyles(styles)(EventCard);
--- src/CardList.js
+++ src/CardList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Card from './Card';
+import EventCard from './Card';
 import moment from 'moment';
+import Grid from '@material-ui/core/Grid';
 
 class CardList extends Component {
   constructor(props) {
@@ -9,15 +10,15 @@ class CardList extends Component {
 
     this.state = {
       events: [],
-      sort: '',
+      error: '',
       lat: null,
       long: null
     };
   }
 
-
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
+      // get the position of the client
       position => {
         this.setState({
           lat: position.coords.latitude,
@@ -25,16 +26,17 @@ class CardList extends Component {
         });
 
         let dateNow = moment().format('YYYY-MM-DDTHH:mm:ssZ');
+        // format the date as ticketmaster required
         axios
           .get(
+            // format the api based on client's position to get the distance API
             `https://app.ticketmaster.com/discovery/v2/events.json?size=20&geoPoint=${
               this.state.lat
-            },${
-              this.state.long
-            }&sort=distance,asc&startDateTime=${dateNow}
+            },${this.state.long}&sort=distance,asc&startDateTime=${dateNow}
             &apikey=r0IzC89Utgdk69QfPEY9Gtpr0zK8drt0`
           )
           .then(res => {
+            // get the top 20 events based on the distance asc order
             const events = res.data._embedded.events;
             console.log(events);
             this.setState({ events });
@@ -43,32 +45,46 @@ class CardList extends Component {
             console.log(err);
           });
       },
-      error => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+      error => this.setState({ error: error.message })
     );
   }
 
   render() {
     const cardComponent = this.state.events.map(event => {
-      const price = typeof event.priceRanges !== 'undefined' ? event.priceRanges[0] : 0;
-      const address = event._embedded.venues[0].address.line1 + event._embedded.venues[0].city.name
-        + event._embedded.venues[0].state.stateCode
+      const price =
+        typeof event.priceRanges !== 'undefined' ? event.priceRanges[0] : 0;
+      //check if the price range is provided: if true then return priceRanges or pass 0 as props
+      const venue = event._embedded.venues[0];
+      const address =
+        //for mate the address as street, city, state
+        venue.address.line1 + venue.city.name + venue.state.stateCode;
+
       return (
-        <Card
+        <EventCard
+          style={{ marginTop: 50 }}
           key={event.id}
           name={event.name}
           time={event.dates.start.localDate}
           distance={event.distance}
           price={price}
-          locationname = {event._embedded.venues[0].name}
-          locationaddress = {address}
+          locationname={venue.name}
+          locationaddress={address}
+          media={event.images[0].url}
         />
       );
     });
 
     return (
       <div>
-        <ul>{cardComponent}</ul>
+        <Grid
+          container
+          spacing={24}
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+        >
+          {cardComponent}
+        </Grid>
       </div>
     );
   }
